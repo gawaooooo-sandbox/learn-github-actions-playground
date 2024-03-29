ef49f70b19882fae76edd3645b835513eaee1e6e
gawao <gawao57@gmail.com>
2024-03-29 11:50:50 +0900

    add nuxt project files

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from "nuxt/config"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    rootDir: 'src',
+    devtools: { enabled: true }
+})
