b5b960dceae4f36e37ac96163c36a91a1f8f40fd
gawao <gawao57@gmail.com>
2024-03-29 14:36:31 +0900

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ import { defineNuxtConfig } from "nuxt/config"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    rootDir: 'src',
+    srcDir: "src",
     devtools: { enabled: true }
 })
