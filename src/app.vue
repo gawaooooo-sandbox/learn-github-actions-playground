1d858438f02f16558abbfc3c38ad5995d309872d
gawao <gawao57@gmail.com>
2024-04-01 16:09:12 +0900

    auto merge test

--- src/app.vue
+++ src/app.vue
@@ -2,11 +2,17 @@
 console.log('-------- print process.env --------');
 console.log(process.env);
 console.log('▲▲▲▲▲▲▲▲ print process.env ▲▲▲▲▲▲▲▲');
+
+// router test
+const router = useRouter();
+
+const nav = [{ label: 'Home', to: '/' }];
 </script>
 
 <template>
     <div>
         <p>testB</p>
         <NuxtWelcome />
+        <NuxtPage />
     </div>
 </template>
